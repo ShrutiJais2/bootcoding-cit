640c317439cfa6ca51d5753c47039b93de17d6fa
ShrutiJais2 <144877196+ShrutiJais2@users.noreply.github.com>
2023-09-13 13:29:23 +0530

    Add files via upload

---
+++ ArithematicProgram.java
@@ -0,0 +1,16 @@
+public class ArithematicProgram 
+{
+public static void main (String args[])
+{
+int a=88 , b=90;
+int  add, sub, mul, div ;
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+System.out.print("Addition : "+ add );
+System.out.print("Substraction : "+ sub );
+System.out.print("Multiplcation : "+ mul);
+System.out.print("Division: "+ div);
+}
+}
